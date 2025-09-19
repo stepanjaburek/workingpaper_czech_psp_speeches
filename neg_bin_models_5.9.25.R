e1834c0e8d49146f3b6a788be3274ced04bc2de8
Stepan Jaburek <stepan.jaburek@seznam.cz>
2025-09-19 13:45:53 +0200

    graphs_fix

--- neg_bin_models_5.9.25.R
+++ neg_bin_models_5.9.25.R
@@ -5,9 +5,10 @@ library(data.table)
 library(marginaleffects)# optional
 library(flexplot) # optional
 library(texreg) # optional
+library(MASS)
 
 #---------------------------------
-data<-fread("ideology_data.csv")
+data<-fread("~/Documents/czech_psp_data/ideology_data.csv")
 
 data_month <- data %>%
   group_by(wing,month) %>% 
@@ -70,64 +71,8 @@ summary(mod.other)
 
 
 eff <- ggpredict(mod.other,  
-  terms = list("right_wing" = seq(0, 1, 1)))
-
-eff %>% plot()
-
-
-
-install.packages("topmodels", repos="http://R-Forge.R-project.org")
-library(topmodels)
-
-
-
-
-
-mod.other <- glm.nb(
- out_ideology_mentions ~ lag_quarterly_inflation +  lag_public_opinion + 
-   election_period +  government +
-   right_wing   +
-   + offset(log_monthly_word_count),
-  data = data_month
+  terms = list("right_wing" = seq(0, 1, 1))
 )
-#Rootogram
-rootogram(mod.other,
-          main = "Hanging rootogram of mod1.pois")
 
-
-mod.self <- glm.nb(
- in_ideology_mentions ~ lag_quarterly_inflation +  lag_public_opinion + 
-   election_period +  government +
-   right_wing   +
-   + offset(log_monthly_word_count),
-  data = data_month
-)
-#Rootogram
-rootogram(mod.self,
-          main = "Hanging rootogram of mod1.pois")
-
-
-
-#Predict outcomes
-data_month$preds_predictors2 <- predict(mod.self, 
-                                newdata = data_month,
-                                #Note that we should not use the argument name here; but we want exponentiated outcomes
-                                "response")
-
-#Plot two overlayed histograms
-data_month %>%
-  ggplot() +
-  #Observed outcomes
-  geom_histogram(aes(x = in_ideology_mentions),
-                 #transparent color
-                 alpha=0.5)+
-  #Predicted outcomes
-  geom_histogram(aes(x = preds_predictors2),
-                 #transparent red color
-                 fill = "red", 
-                 alpha =  0.3)+
-  #ggtheme
-  theme_minimal() +
-  ggtitle("Distribution of observed vs. predicted outcomes") +
-  ylab("Number of MEPs") +
-  xlab("Number of reports (legislative proposals)")
+eff %>% 
+  plot()
